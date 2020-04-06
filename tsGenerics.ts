35ff305853717009c0d45b88d5131dc513333e19
yc yap <yowchun93@gmail.com>
2020-04-06 11:46:58 +0800

    Practice generics again

--- tsGenerics.ts
+++ tsGenerics.ts
@@ -1,32 +1,25 @@
-
-// generic array
-type numArray = Array<number>
-
 const last = <T>(arr: Array<T>): T => {
-  return arr[arr.length - 1 ];
+  return arr[arr.length - 1]
 }
 
-const l = last([1,2,3]);
-const l2 = last(['a', 'b', 'c']);
+console.log(last([1, 2, 3]))
+console.log(last(['a', 'b', 'c']))
 
 const makeArr = <X, Y>(x: X, y: Y): [X, Y] => {
-  return [x, y];
+  return [x, y]
 }
 
-const v = makeArr(5, 6);
-const v2 = makeArr("a", "b");
+// i like how the editor tells me the type of arr1 and arr2
+const arr1 = makeArr(1, 'a')
+const arr2 = makeArr('a', 1)
 
-// make function accept object with firstName and lastName
-// const makeFullName = (obj: {firstName: string, lastName: string}) => {
-//   return {
-//     ...obj,
-//     fullName: obj.firstName + ' ' + obj.lastName
-//   }
-// }
+interface UserName {
+  firstName: string
+  lastName: string
+  fullName?: string
+}
 
-// i think i kind of get it know, generics allow you to declare a type?
-// can this be done using interface instead ?
-const makeFullName = <T extends { firstName: string; lastName: string }>(obj: T) => {
+const makeFullName = (obj: UserName): UserName => {
   return {
     ...obj,
     fullName: obj.firstName + " " + obj.lastName
@@ -34,13 +27,3 @@ const makeFullName = <T extends { firstName: string; lastName: string }>(obj: T)
 }
 
 const yc = makeFullName({firstName: 'yap', lastName: 'yowchun'})
-const yc2 = makeFullName({ firstName: "bob", lastName: "junior", age: 15 })
-
-interface Tab<T> {
-  id: string;
-  position: number;
-  data: T;
-}
-
-type NumberTab = Tab<number>;
-type StringTab = Tab<string>;
